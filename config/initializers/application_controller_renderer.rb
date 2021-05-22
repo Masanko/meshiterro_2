deab8cd7d7d642164def794c0ddb54a2e2609063
Masanko <dwrys431@gmail.com>
2021-05-22 00:06:35 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cc81e0dee9572933c39b0a00b100eea5b5a6486ca7428425705653d34ee4d30c7ba63fefb6a98114a0070cc58b6358cb8baecbfe3681d468fd8fc1ecb1fc759f'
